a44f22968f20c4872b9b596d0b5a2155983a3aeb
SuperCheese21 <supercheese21@comcast.net>
2018-07-11 23:39:30 -0700

    Merged server files, fixed file paths

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,84 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const createError = require('http-errors');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const getData = require('./api.js');
+
+const app = express();
+
+const DEFAULT_ICAO = 'KPDX';
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('map', {
+        icao: DEFAULT_ICAO
+    });
+});
+
+app.get('/:icao', (req, res, next) => {
+    const icao = req.params.icao.toUpperCase();
+    const routes = getData.routes(icao);
+    if (routes) {
+        res.render('map', {
+            icao: icao
+        });
+    }
+    else next();
+});
+
+app.get('/api/routes/:icao', (req, res, next) => {
+    const icao = req.params.icao.toUpperCase();
+    const routes = getData.routes(icao);
+
+    if (routes) res.status(200).json(routes);
+    else next();
+});
+
+app.get('/api/coords/:type/:icao', (req, res, next) => {
+    const icao = req.params.icao.toUpperCase();
+    const type = req.params.type;
+    const coords = getData.coords(icao, type);
+
+    if (coords) res.status(200).json(coords);
+    else next();
+});
+
+app.get('/api/airlines/:icao1/:icao2', (req, res, next) => {
+    const icao1 = req.params.icao1.toUpperCase();
+    const icao2 = req.params.icao2.toUpperCase();
+    const airlines = getData.airlines(icao1, icao2);
+
+    if (airlines) res.status(200).json(airlines);
+    else next();
+});
+
+app.get('*', (req, res, next) => {
+    next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+exports.app = functions.https.onRequest(app);
